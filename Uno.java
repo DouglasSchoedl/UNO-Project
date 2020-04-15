bf28816c65aeb7c24aa665f8ce5a621a4dada312
Douglas Schoedl <schoedld@MSI.localdomain>
2020-04-14 23:11:36 -0400

    Made alot of changes here. Might need to look over in detail but.. im getting the game logic in place. Reverse fully works i think, Draw2 i need to work on more if we want to stack multiple draw2's. I am working on Skip, Wild, and Draw4Wild in another test.java file so i have that covered. But its starting to all come together... yea.

--- Uno.java
+++ Uno.java
@@ -3,64 +3,93 @@ import java.util.Scanner;
 public class Uno
 {
 	private static Deck d;
-	private static Player one;
-	private static Player two;
+	private static int PSIZE = 2;	
+	private static Player [] player = new Player[PSIZE];
+	private static Card leading; //Top of discard pile
+
 
 	public static void main(String[] args)
 	{
 		//Creating Deck of cards and the two players
 		d = new Deck();
-		one = new HumanPlayer(1);
-		two = new HumanPlayer(2);
 
-		one.dealCards(d);
-		two.dealCards(d);
+		for(int i=0; i<PSIZE; i++)
+		{
+			player[i] = new HumanPlayer(i+1);
+			player[i].dealCards(d);
+		}
 
 		//Leading with the first card from the pickup stack. This is 3 lines for printing for testing
-		Card leading = d.PickupCard();			//picking up the leading card in the PStack
+		leading = d.PickupCard();			//picking up the leading card in the PStack
 		d.DiscardCard(leading);					//putting the card into the discard pile
+		
+		while(leading.getSuit() == Suit.WILD)	//leading card cannot be wild
+		{
+			leading = d.PickupCard();
+			d.DiscardCard(leading); 
+		}
+
 		System.out.println("the first card is: " + leading.printCard());
+		int index = 0;	//start with player 1
+		Scanner input = new Scanner(System.in);
+
 
 		//while neither is out of cards, loop for testing
-		while(one.NumCardsInHand() != 0 && two.NumCardsInHand() != 0)
+		while(player[index].NumCardsInHand() != 0)
 		{
-			one.ShowHand();
-			System.out.println("Player 1, play a card.");
-			//Input from console
-			Scanner input = new Scanner(System.in);
-			int choice = input.nextInt();
-
 		
+			player[index].ShowHand();
+			leading = d.DiscardPile[d.DPsize()-1];
+			System.out.println("DiscardTop: " + leading.printCard());	
+			System.out.printf("Player %d, play a card or put -1 to draw.\n>", player[index].getPnum());
+
+			int choice = input.nextInt();
+			if(choice == -1)
+			{
+				DrawUntilPlayable(player[index],d,leading);
+				continue;
+			}
+			
 			//testing the players choice vs. the top card of discard stack
-			while(!isDiscardable(d.DiscardPile[d.DPsize()-1], one.getCard(choice)))
+			if(!isDiscardable(leading, player[index].getCard(choice)))
 			{
 				System.out.println("That card cannot be played. Please choose another card.");
-				choice = input.nextInt();
+				continue;	
 			}
-			one.Discard(d, choice);
 			
-			if(one.NumCardsInHand() == 0)	//this triggers if player one played last card
+			if(player[index].getCard(choice).getType() == TypeOfCard.REVERSE)
+			{
+				player[index].Discard(d, choice);
+				Reverse(index);	
+			}		
+			else if(player[index].getCard(choice).getType() == TypeOfCard.DRAW2)
+			{
+				player[index].Discard(d, choice);
+				index = NextTurn(index);
+				DrawTwo(player[index],d);
+				System.out.printf("Player %d, had to draw 2 and lost a turn\n", player[index].getPnum());
+			}
+			else
+				player[index].Discard(d, choice);
+
+
+
+			if(player[index].NumCardsInHand() == 0)	//this triggers if player[i] played last card
 				break;
 
-			two.ShowHand();
-			System.out.println("Player 2, play a card.");
-			choice = input.nextInt();
-			while(!isDiscardable(d.DiscardPile[d.DPsize()-1], two.getCard(choice)))
-			{
-				System.out.println("That card cannot be played. Please choose another card.");
-				choice = input.nextInt();
-			}
-			two.Discard(d, choice);
+			index = NextTurn(index);
+			
 
 		}
 	}
 
-
+	
+	//-----------------------------------------------------------------------------
 	//tests if a card is able to be played based on rules of the game
 	public static boolean isDiscardable(Card top, Card test)
 	{
 		boolean discardable = false;	//will not be discardable if it doesn't satisfy
-										//the below conditions to be playable
+		//the below conditions to be playable
 		if(test.getSuit() == Suit.WILD)	//if it's wild, it's always able to be played.
 			discardable = true;
 		else if(test.getSuit() == top.getSuit())	//Cards must be the same suit to be played
@@ -72,7 +101,83 @@ public class Uno
 
 	}
 
+		
+	//-----------------------------------------------------------------------------
+	public static int NextTurn(int index)
+	{
+		if(index == PSIZE-1)
+			index = 0;
+		else
+			index++;
+		return index;
+	}
 
+
+
+
+
+	//-----------------------------------------------------------------------------
+	//Have to add wait functionallity to allow for multiple drawtwo's
+	public static void DrawTwo(Player pnum, Deck D)
+	{
+		pnum.TakeCard(D);
+		pnum.TakeCard(D);
+	}
+	public static void DrawTwos(Player pnum, Deck D, int numdrawtwos)
+	{
+		System.out.printf("Player %d had to draw %d cards!", pnum.getPnum(), (numdrawtwos*2));
+		while(numdrawtwos-- != 0)
+		{
+			DrawTwo(pnum, D);
+		}
+	}
+
+	//-----------------------------------------------------------------------------
 	
-	
+
+	//a bit overly complicated but reverse player array starting at the index
+	//Example {0,1,2,3} at index 2 -> {0,3,2,1}
+	//So player 2 plays reverse then the next is player 1 then 0 then..
+	//Example {0,3,2,1} at index 1 -> {2,3,0,1}
+	public static void Reverse(int index)
+	{
+		int num;
+		if(index == 0)
+			num = PSIZE-1;
+		else
+			num = index-1;
+		int i = index;
+		Player[] temp = new Player[PSIZE];
+		while(i != num)
+		{
+			temp[i] = player[index];
+			if(i == PSIZE-1)
+				i=0;
+			else
+				i++;
+			if(index == 0)
+				index = PSIZE-1;
+			else
+				index--;
+		}
+		temp[i] = player[index];
+
+		player = temp;
+	}
+
+
+	//-----------------------------------------------------------------------------
+	//Draws until playable
+	public static void DrawUntilPlayable(Player p, Deck D, Card leading)
+	{
+		Card test = p.TakeCard(D);
+		while(!isDiscardable(leading, test))
+		{
+			test = p.TakeCard(D);
+		} 
+	}
+
+
+	//-----------------------------------------------------------------------------
+
 }
