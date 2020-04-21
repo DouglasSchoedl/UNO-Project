cafa2791dc99847a124337c069b62731331e9cd0
jkk39 <jeffkk@me.com>
2020-04-21 14:25:11 -0400

    Update CardPane.java
    added ability for scaling of cards to use better images

--- CardPane.java
+++ CardPane.java
@@ -3,6 +3,9 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.event.*;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.RenderingHints;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.Icon;
@@ -14,6 +17,7 @@ class CardPane extends JPanel
 {
 	private Card card;
 	private String cardFileName;
+	private ImageIcon cardicon;
 	
 	public CardPane(Card c)
 	{
@@ -21,12 +25,23 @@ class CardPane extends JPanel
 		card = c;
 		cardFileName = card.getSuit().toString() + card.getType().toString() + ".png";
 
-		JLabel cardlabel = new JLabel();
-		Icon card = new ImageIcon(getClass().getResource("images/" + cardFileName));
-		cardlabel.setIcon(card);
-		cardlabel.setHorizontalAlignment(SwingConstants.CENTER);
-		add(cardlabel);
+		cardicon = new ImageIcon(getClass().getResource("images/" + cardFileName));
+	}
+	
+	protected void paintComponent(Graphics g) {
+	    BufferedImage scaledImage = getScaledImage();
+	    super.paintComponent(g);
+	    g.drawImage(scaledImage, 0, 0, null);
+	}
 
+	private BufferedImage getScaledImage()
+	{
+	    BufferedImage image = new BufferedImage(96, 144, BufferedImage.TYPE_INT_ARGB);
+	    Graphics2D g2d = (Graphics2D) image.createGraphics();
+	    g2d.addRenderingHints(new RenderingHints(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY));
+	    g2d.drawImage(cardicon.getImage(), 0, 0, 96, 144, null);
+
+	    return image;
 	}
 
 	public Card getCard()
